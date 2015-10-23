10f462e78d7d4e72fe01d8e94c544864fe9377d0
TzuChanChuang <alice20041025@yahoo.com.tw>
2015-10-23 16:35:55 +0800

    Click on an object to link to a website

---
+++ ClicktoWebsite.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClicktoWebsite : MonoBehaviour {
+	
+	public string url = "https://developer.vuforia.com/";
+	void OnMouseOver() {
+		if (Input.GetMouseButtonDown(0) ||  (Input.touchCount >  0))
+			Application.OpenURL(url);
+	}
+}
